f0ce31d282c2e7ce623565de946c6d3347a45ddf
jstrathres <jstraz7@gmail.com>
2023-02-28 20:41:04 -0500

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,108 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularToDo';
+
+  tasks:Todo[] =
+  [
+    {
+      task: "Laundry",
+      completed: true
+    },
+    {
+      task: "Air Tires",
+      completed: false
+    },
+    {
+      task: "Replace Water Heater",
+      completed: true
+    },
+    {
+      task: "Fix Trim",
+      completed: false
+    },
+    {
+      task: "Wash Dog",
+      completed: true
+    },
+    {
+      task: "Recycle Boxes",
+      completed: false
+    },
+    {
+      task: "Organize Shoes",
+      completed: true
+    },
+    {
+      task: "Airdust PC",
+      completed: false
+    },
+    {
+      task: "Clear Out Fridge",
+      completed: true
+    },
+    {
+      task: "Clean Room",
+      completed: false
+    },
+    {
+      task: "Make Dinner",
+      completed: true
+    },
+    {
+      task: "Unload Dishwasher",
+      completed: false
+    },
+  ];
+
+ newTask:string= "";
+ filter:string = "";
+
+  addTask() :void
+  {
+    let result:Todo = 
+    {
+      task: this.newTask,
+      completed: false,
+    }
+    this.tasks.push(result);    
+  }
+
+  removeTask(index:number):void 
+  {
+    this.tasks.splice(index,1)
+  }
+
+  CompleteTask(index:number):void{
+    this.tasks[this.getCorrectIndex(index)].completed = true;
+  }
+  
+  IsCompleted():boolean 
+  {
+    if(this.tasks.length ==0) 
+    {
+      return true;
+    }
+
+    let result : boolean = true;
+    this.tasks.forEach((t:Todo) =>
+    {
+      if(t.completed == false)
+      {
+        result =false;
+      }
+    });
+    return result;
+  }
+
+  getFiltered():Todo[]{
+    return this.tasks.filter((t:Todo) => t.task.includes(this.filter));
+  }
+
+  getCorrectIndex(index:number):number{
+    //index is filtered, we need the original
+    let task:Todo = this.getFiltered()[index];
+
+    return this.tasks.findIndex((t:Todo) => t.task == task.task && t.completed == task.completed);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent
   ],
   imports: [
+    // This FormsModule Must be Added!
+    FormsModule,
     BrowserModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
